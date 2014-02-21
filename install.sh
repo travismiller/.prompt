e8649d28740778093fb6cf3098b3c786c5cb712b
Travis Miller <puckbag@gmail.com>
2014-02-21 03:31:36 -0600

    installer update

--- install.sh
+++ install.sh
@@ -2,18 +2,23 @@
 
 CWD=`pwd`
 
-# http://stackoverflow.com/a/7292662/1596013
-git --version 2>&1 >/dev/null
-GIT_IS_AVAILABLE=$?
-if [ $GIT_IS_AVAILABLE -eq 0 ]; then
-	git clone https://github.com/puckbag/.prompt.git ~/.prompt
-else
-	curl -sS https://github.com/puckbag/.prompt/archive/master.zip > ~/.prompt.zip
-	cd ~
-	unzip ~/.prompt.zip
-	rm ~/.prompt.zip
+if [ ! -d ~/.prompt ]; then
+	# http://stackoverflow.com/a/7292662/1596013
+	git --version 2>&1 >/dev/null
+	GIT_IS_AVAILABLE=$?
+	if [ $GIT_IS_AVAILABLE -eq 0 ]; then
+		if [ -d ~/.prompt ]; then
+			sleep 0
+		else
+			git clone https://github.com/puckbag/.prompt.git ~/.prompt
+		fi
+	else
+		curl -sS https://github.com/puckbag/.prompt/archive/master.zip > ~/.prompt.zip
+		cd ~
+		unzip ~/.prompt.zip
+		rm ~/.prompt.zip
+	fi
 fi
-
 
 if grep -q '~/.prompt/profile' ~/.profile ~/.bashrc 2>/dev/null; then
 	sleep 0
